1427b01c957583c695f8bb0626ff1f1028d364a1
Hash3816 <agafoncevdima962@gmail.com>
2025-04-12 20:13:21 +1000

    Оптимизировал 3 задание

--- Zadanie_3.go
+++ Zadanie_3.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 //type Node struct {
 //	next  *Node
 //	Value rune
-//}
+//
 
 //type Odnosvyaz struct {
 //	begin  *Node
@@ -17,11 +15,22 @@ import (
 //func Create_sp() *Odnosvyaz {
 //	return &Odnosvyaz{begin: nil, length: 0}
 //}
+
+//func (sp *Odnosvyaz) Print_sp() {
+//	if sp.length == 0 {
+//		fmt.Println("Список пуст.")
+//	}
+//	now_node := sp.begin
+//	for now_node != nil {
+//		fmt.Println(string(now_node.Value))
+//		now_node = now_node.next
+//	}
+//}
+
 func (sp *Odnosvyaz) Add(value rune) {
 	new_node := &Node{next: nil, Value: value}
 	if sp.length == 0 {
 		sp.begin = new_node
-	}
 		sp.length++
 	} else {
 		node := sp.begin
@@ -33,45 +42,80 @@ func (sp *Odnosvyaz) Add(value rune) {
 	}
 }
 
-func (sp *Odnosvyaz) Count_el_3() bool {
-	el_nums_sp := Create_sp()
-	now_node := sp.begin
+func (sp *Odnosvyaz) get_el(ind int) rune {
+	node := sp.begin
+	for i := 0; i < ind; i++ {
+		node = node.next
+	}
+	return node.Value
+}
 
-	for i := 0; i < sp.length; i++ {
-		value_1 := now_node.Value
-		now_node = now_node.next
-		el_node := el_nums_sp.begin
+func (sp *Odnosvyaz) Swap(ind1 int, ind2 int) {
+	memory_link_1 := sp.begin
+	memory_link_2 := sp.begin
 
-		counter := 0
-		for j := 0; j < el_nums_sp.length; j++ {
-			value_2 := el_node.Value
-			el_node = el_node.next
-			if value_1 == value_2 {
-				counter++
-			}
+	if ind1 == ind2 {
+		return
+	}
+	if ind1 > ind2 {
+		for i := 0; i < ind2; i++ {
+			memory_link_1 = memory_link_1.next
+			memory_link_2 = memory_link_2.next
 		}
-		if counter == 0 {
-			el_nums_sp.Add(value_1)
+		for i := ind2; i < ind1; i++ {
+			memory_link_1 = memory_link_1.next
+		}
+	} else {
+		for i := 0; i < ind1; i++ {
+			memory_link_1 = memory_link_1.next
+			memory_link_2 = memory_link_2.next
+		}
+		for i := ind1; i < ind2; i++ {
+			memory_link_2 = memory_link_2.next
 		}
 	}
+	bufer := memory_link_1.Value
+	memory_link_1.Value = memory_link_2.Value
+	memory_link_2.Value = bufer
 
-	el_node := el_nums_sp.begin
+}
+func (list *Odnosvyaz) HeapSort() {
+	n := list.length
+	for i := n/2 - 1; i >= 0; i-- {
+		heapify(list, n, i)
+	}
+}
 
-	for i := 0; i < el_nums_sp.length; i++ {
-		counter := 0
-		value_el_node := el_node.Value
-		el_node = el_node.next
-		sp1_node := sp.begin
+func heapify(list *Odnosvyaz, n int, i int) {
+	largest := i
+	left := 2*i + 1
+	right := 2*i + 2
 
-		for j := 0; j < sp.length; j++ {
-			value_old_sp := sp1_node.Value
-			sp1_node = sp1_node.next
-			if value_el_node == value_old_sp {
-				counter++
+	if left < n && list.get_el(left) > list.get_el(largest) {
+		largest = left
+	}
+	if right < n && list.get_el(right) > list.get_el(largest) {
+		largest = right
+	}
+	if largest != i {
+		list.Swap(i, largest)
+		heapify(list, largest, i+1)
+	}
+}
+
+func (sp *Odnosvyaz) Count_el_3() bool {
+	sp.HeapSort()
+	counter := 1
+
+	for i := 0; i < sp.length-1; i++ {
+		if sp.get_el(i) == sp.get_el(i+1) {
+			counter++
+			if counter == 3 {
+				return true
 			}
-		}
-		if counter == 3 {
-			return true
+
+		} else {
+			counter = 1
 		}
 	}
 	return false
